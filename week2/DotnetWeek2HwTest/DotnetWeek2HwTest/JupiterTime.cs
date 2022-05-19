46054a4a7ccb0f1e70417a61abf1891ea677e134
ReenuMary <reenumaryabraham@gmail.com>
2022-05-19 09:52:32 +0200

    Jupiter time add with xunit test

---
+++ week2/DotnetWeek2HwTest/DotnetWeek2HwTest/JupiterTime.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace DotnetWeek2HwTest
+{
+    public class JupiterTime
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+
+        public JupiterTime(int hours, int minutes)
+        {
+            if(hours > 0  && minutes > 0)
+            {
+                Minutes = minutes;
+                Hours = hours;
+                if (minutes > 60)
+                {
+                    hours += minutes / 60;
+                    minutes = minutes % 60;
+                }
+                if (hours >= 10)
+                {
+                    hours = hours % 10;
+                } 
+            }
+           else
+            {
+                throw new ArgumentException("Hours and minutes cannot be negative");
+            }
+
+           
+        }
+
+        public JupiterTime AddHours(int hours, out string day)
+        {
+            JupiterTime calculatedTime = this;
+            calculatedTime.Hours += hours;
+            day = "the same day";
+
+        if (calculatedTime.Hours < 0)
+            {
+                day = $"{ (calculatedTime.Hours / -10) + 1 } day/s before";
+                calculatedTime.Hours = 10 + calculatedTime.Hours % 10;
+            }
+            else if (calculatedTime.Hours >= 10)
+            {
+                day = $"{ calculatedTime.Hours / 10} day/s after";
+                calculatedTime.Hours = calculatedTime.Hours % 10;
+            }
+           
+
+            return calculatedTime;
+        }
+
+        public JupiterTime AddMinutes(int minutes, out string day)
+        {
+            JupiterTime calculatedTime = this;
+            day = "the same day";
+            calculatedTime.Minutes += minutes;
+
+            if (calculatedTime.Minutes <= -60 || calculatedTime.Minutes >= 60)
+            {
+               calculatedTime.Hours = calculatedTime.AddHours(calculatedTime.Minutes / 60, out day).Hours;
+               calculatedTime.Minutes = calculatedTime.Minutes % 60;
+            }
+            if (calculatedTime.Minutes < 0)
+            {
+                calculatedTime.Minutes = (60+ calculatedTime.Minutes );
+                calculatedTime.AddHours(-1,out day);
+            }
+
+            return calculatedTime;
+        }
+    }
+}
+
--- week2/DotnetWeek2HwTest/DotnetWeek2HwTest/Program.cs
+++ week2/DotnetWeek2HwTest/DotnetWeek2HwTest/Program.cs
@@ -6,7 +6,36 @@ namespace DotnetWeek2HwTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  var time = new JupiterTime(14,88);
+            //time.Hours = 8;
+            //time.Minutes = 4;
+            //PrintTime(time);
+            string day = string.Empty;
+            var time = new JupiterTime(2, 20);
+             var timeAfterAddingHours = time.AddHours(-26,out  day);
+             PrintTime(timeAfterAddingHours);
+             PrintMessage(day);
+
+         /*   var timeAfterAddingMinutes = time.AddMinutes(-145,out day);
+            PrintTime(timeAfterAddingMinutes);
+            PrintMessage(day);*/
+        }
+
+        static void PrintTime(JupiterTime time)
+        {
+            if(time.Minutes<10)
+            {
+                Console.WriteLine($"Jupiter time is {time.Hours}:0{time.Minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"Jupiter time is {time.Hours}:{time.Minutes}");
+            }
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
