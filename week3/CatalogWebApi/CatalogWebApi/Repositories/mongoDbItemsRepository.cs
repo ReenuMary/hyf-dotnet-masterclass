37c3fe8858c119b7704102ce1514e0fa8f144ebd
ReenuMary <reenumaryabraham@gmail.com>
2022-05-27 16:06:22 +0200

    get, put , del for mongodbItems repo

--- week3/CatalogWebApi/CatalogWebApi/Repositories/mongoDbItemsRepository.cs
+++ week3/CatalogWebApi/CatalogWebApi/Repositories/mongoDbItemsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CatalogWebApi.Entities;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace CatalogWebApi.Repositories
@@ -9,6 +10,7 @@ namespace CatalogWebApi.Repositories
         private const string databaseName = "catalog";
         private const string collectionName = "items";
         private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
 
         public mongoDbItemsRepository(IMongoClient mongoClient)
@@ -29,17 +31,19 @@ namespace CatalogWebApi.Repositories
 
         public Item? GetItem(Guid id)
         {
-            throw new NotImplementedException();
+           var filter = filterBuilder.Eq(x => x.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
         }
 
         public IEnumerable<Item> GetItems()
         {
-            throw new NotImplementedException();
+            return itemsCollection.Find(new BsonDocument()).ToList();
         }
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(x => x.Id, item.Id);
+            itemsCollection.FindOneAndReplace(filter, item);
         }
     }
 }
